6ca2c5de4a72ee55093b5f140716b23f50845bdd
Sandhep <b.sandhep111@gmail.com>
2024-08-15 19:00:58 +0530

    simple server

---
+++ server-1/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Set up a route to respond to GET requests to the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Make the server listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
