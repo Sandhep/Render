205b7d55eb9b130011dfcacbe317178db7984651
Sandhep <b.sandhep111@gmail.com>
2024-08-15 20:46:52 +0530

    server v2

--- server-1/server.js
+++ server-1/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 // Set up a route to respond to GET requests to the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World!..!!!');
+    res.send('Hii Shobika');
 });
 
 // Make the server listen on the specified port
