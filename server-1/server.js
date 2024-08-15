3c6cbf501f6a44e16f39b8982d6a119b7d1ffb64
Sandhep <b.sandhep111@gmail.com>
2024-08-15 20:58:09 +0530

    server v3

--- server-1/server.js
+++ server-1/server.js
@@ -4,9 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const now = new Date();
+
+const date = now.toDateString(); // Converts to a readable date string
+const time = now.toTimeString(); // Converts to a readable time string
+
+
 // Set up a route to respond to GET requests to the root URL
 app.get('/', (req, res) => {
-    res.send('Hii Shobika');
+    res.send(`Hii Shobika !!   Time:${time}`);
 });
 
 // Make the server listen on the specified port
